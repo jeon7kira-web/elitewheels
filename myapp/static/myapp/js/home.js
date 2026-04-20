12c7e71e43535dca3f90ec0603ad3d404db85c9b
jeon7kira-web <aresmoukmo@gmail.com>
2026-04-20 23:37:53 +0200

    all home page ui

--- myapp/static/myapp/js/home.js
+++ myapp/static/myapp/js/home.js
@@ -1,17 +1,42 @@
 const cars = document.querySelectorAll(".car");
-const carcontainer = document.querySelector(".car-container");
+const carcontainer = document.querySelector(".cars-container");
 
 function setActive(car) {
   cars.forEach((c) => c.classList.remove("active"));
   car.classList.add("active");
 }
 
-cars.forEach((car) => {
-  car.addEventListener("mouseenter", () => {
-    setActive(car);
+if (carcontainer) {
+  cars.forEach((car) => {
+    car.addEventListener("mouseenter", () => {
+      setActive(car);
+    });
   });
-});
 
-container.addEventListener("mouseleave", () => {
-  setActive(cars[0]); // first main car
-});
+  carcontainer.addEventListener("mouseleave", () => {
+    setActive(cars[0]);
+  });
+}
+
+// Reviews slider
+const reviewCards = document.querySelectorAll(".reviews__card");
+let reviewCurrent = 0;
+
+function changeSlide(dir) {
+  if (!reviewCards.length) return;
+  reviewCards[reviewCurrent].classList.remove("active");
+  reviewCurrent =
+    (reviewCurrent + dir + reviewCards.length) % reviewCards.length;
+  reviewCards[reviewCurrent].classList.add("active");
+}
+
+if (reviewCards.length) {
+  setInterval(() => changeSlide(1), 3500);
+}
+
+document
+  .querySelector(".reviews__arrow--left")
+  ?.addEventListener("click", () => changeSlide(-1));
+document
+  .querySelector(".reviews__arrow--right")
+  ?.addEventListener("click", () => changeSlide(1));
